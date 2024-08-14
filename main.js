87896777574495a1b2a9e5c0636080252b381629
jimmy-clenio <jimmyc@prof.educacao.sp.gov.br>
2024-08-14 14:36:56 -0300

    construção do main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuario = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuario == ""){
+    nomeUsuario = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuario == null){
+    elemento.textContent = " seja muito bem-vindo.";
+}else{
+    elemento.textContent = nomeUsuario;
+}
+
